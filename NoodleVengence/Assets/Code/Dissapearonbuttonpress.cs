a3c6c99b46cb2a6ca2fbc2c548232ddfe5d21b7b
epicpython <jr3python@gmail.com>
2017-09-14 22:49:47 -0400

    added hjkl press input
    press h j k or l to destroy a lamp
    
    what fun! :)

--- NoodleVengence/Assets/Code/Dissapearonbuttonpress.cs
+++ NoodleVengence/Assets/Code/Dissapearonbuttonpress.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class Dissapearonbuttonpress : MonoBehaviour {
 
+	public string letter;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		if (letter == "h")
+		{
+			if (Input.GetKeyDown (KeyCode.H)) 
+			{
+				Debug.Log ("pressed H");
+				Destroy (gameObject);
+			}
+		}else if (letter == "j")
+		{
+			if (Input.GetKeyDown (KeyCode.J)) 
+			{
+				Debug.Log ("pressed J");
+				Destroy (gameObject);
+			}
+		}else if (letter == "k")
+		{
+			if (Input.GetKeyDown (KeyCode.K)) 
+			{
+				Debug.Log ("pressed K");
+				Destroy (gameObject);
+			}
+		} else if (letter == "l")
+		{
+			if (Input.GetKeyDown (KeyCode.L)) 
+			{
+				Debug.Log ("pressed L");
+				Destroy (gameObject);
+			}
+		}
 		
 	}
 }
